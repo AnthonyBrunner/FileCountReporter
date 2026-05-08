a9d21f4735dc2191b1bf7054ee2fb6231db6ea37
AnthonyBrunner <abrunner5361@students.cptc.edu>
2026-05-07 19:55:58 -0700

    Added in controls for a rough layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(86, 31);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(32, 114);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(349, 207);
+            dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(86, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(295, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(267, 31);
+            button2.Name = "button2";
+            button2.Size = new Size(114, 23);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(427, 348);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button2;
     }
 }
